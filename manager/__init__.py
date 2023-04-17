90ca312e2b4b41558bd19335a5cafb0ea60ca62c
Myoko <92731952+MyokoJP@users.noreply.github.com>
2023-04-17 22:44:29 +0900

    Clean: ファイルの移動

--- manager/__init__.py
+++ manager/__init__.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'battleearth'
 socketio = SocketIO(app, cors_allowed_origins='*')
 import manager.main
+import manager.FileExplorer
+import manager.gnuscreen_reader
+import manager.ServerJob
 
 if __name__ == "__main__":
     app.run(port=5000)
